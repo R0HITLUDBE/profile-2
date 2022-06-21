c6ff95fd3d7dd520e246b23eaf42283d48598b22
ROHIT LUDBE <rohitludbe290@gmail.com>
2022-06-21 17:21:28 +0530

    seo add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { BrowserRouter as Router } from "react-router-dom";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
@@ -8,6 +9,17 @@ import Skills from "./components/Skills";
 function App() {
   return (
     <Router>
+      <Helmet>
+        <title>Rohit Ludbe</title>
+        <meta
+          name="description"
+          content="My name is Rohit Ludbe and I am a Web and Android developer from Mumbai."
+        />
+        <meta
+          name="keywords"
+          content="Rohit Ludbe ,Portfolio, Personal Website, Resume, Web Developer, Android Developer, ReactJS"
+        />
+      </Helmet>
       <div className="App">
         <Navbar />
         <Hero />
